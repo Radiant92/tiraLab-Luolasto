39bc745660bb72d01b791b1a9313b65a637880ff
Martin Strohm <martin.strohm@helsinki.fi>
2019-05-05 14:33:24 +0300

    UI valmis

--- tiralabra-cave/src/main/java/cave/ui/Ui.java
+++ tiralabra-cave/src/main/java/cave/ui/Ui.java
@@ -4,51 +4,136 @@ import cave.domain.*;
 import cave.util.MyList;
 import java.awt.Color;
 
+import javafx.application.Application;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+
 /**
- * The user interface class will provide options on how the caverns will be
- * created. For now the user interface automatically creates a list of sleeves
- * which contain rooms and then calls the Draw class to draw a crude visual of
- * their placement. The user interface chooses the rooms randomly and later the
- * number of rooms can be fixed. adjusting the deep value let's you control the
- * depth of the cave in increments of 100. currently it is fixed as 200 until i
- * finish the user interface and path drawing.
+ * The user interface class provides options on how the caverns will be created.
+ * The user interface automatically creates a list of sleeves which are chosen
+ * at random and then populated by rooms. Adjusting the deep value let's you
+ * control the depth of the cave in increments of 100. After choosing the depth
+ * of the cavern the user can choose to either see a visualisation of the
+ * building process (rooms main=red sub=green and paths main=blue sub=pink), or
+ * opt to simply see the stats of said depth.
  *
  * @author strohm
  */
-public class Ui {
+public class Ui extends Application {
+
+    private int deep;
 
     public static void main(String[] args) {
-        int deep = 200;
+        launch(args);
+    }
 
+    public void start(Stage window) {
+        deep = 100;
+        BorderPane pane = new BorderPane();
+
+        Button add100 = new Button("+100");
+        Button add1K = new Button("+1k");
+        Button add10K = new Button("+10k");
+        Button add100K = new Button("+100k");
+        Button sub = new Button("-100");
+        Button activate = new Button("Activate");
+        Button noDraw = new Button("dont draw");
+
+        TextField depth = new TextField("100");
+        depth.setEditable(false);
+
+        TextField mainCavernTime = new TextField("0");
+        TextField subCavernTime = new TextField("0");
+        TextField totalTime = new TextField("0");
+        mainCavernTime.setEditable(false);
+        subCavernTime.setEditable(false);
+        totalTime.setEditable(false);
+
+        Label mainLabel = new Label("Main-cavern build time/sec");
+        Label subLabel = new Label("Sub-cavern build time/sec");
+        Label totalLabel = new Label("Total build time/sec");
+        Label explanation = new Label("Increase/Decrease depth (min 100).\n"
+                + "choose 'activate' to see a visual representation of the cavern"
+                + " or 'dont draw' to just see the stats of the built cavern.\n\n"
+                + "red room = main room \n"
+                + "green room = sub room \n"
+                + "blue lines = main path \n"
+                + "pink lines = sub path");
+
+        HBox setDepth = new HBox(20);
+        setDepth.getChildren().addAll(sub, depth, add100, add1K, add10K, add100K, activate, noDraw);
+
+        VBox results = new VBox(10);
+        results.getChildren().addAll(mainLabel, mainCavernTime, subLabel, subCavernTime, totalLabel, totalTime, explanation);
+
+        add100.setOnAction((event) -> {
+            deep += 100;
+            depth.setText(deep + "");
+        });
+        add1K.setOnAction((event) -> {
+            deep += 1000;
+            depth.setText(deep + "");
+        });
+        add10K.setOnAction((event) -> {
+            deep += 10000;
+            depth.setText(deep + "");
+        });
+        add100K.setOnAction((event) -> {
+            deep += 100000;
+            depth.setText(deep + "");
+        });
+        sub.setOnAction((event) -> {
+            if (deep > 100) {
+                deep -= 100;
+                depth.setText(deep + "");
+            }
+        });
+        activate.setOnAction((event) -> {
+            createCave(deep, true, mainCavernTime, subCavernTime, totalTime);
+        });
+        noDraw.setOnAction((event) -> {
+            createCave(deep, false, mainCavernTime, subCavernTime, totalTime);
+        });
+
+        pane.setTop(setDepth);
+        pane.setCenter(results);
+
+        Scene scene = new Scene(pane, 800, 800);
+
+        window.setScene(scene);
+        window.setTitle("Cave");
+        window.show();
+    }
+
+    public static void createCave(int deep, boolean draw, TextField main, TextField sub, TextField total) {
         CaveMapper cMapper = new CaveMapper(deep);
-        Draw d = new Draw(1920, 1200, "Cavern");
 
         long time = System.currentTimeMillis();
         MyList mainCaves = cMapper.mainCaves();
         time = System.currentTimeMillis() - time;
-        System.out.println("time " + time);
-        d.drawRooms(Color.RED, mainCaves);
+        double seconds = (double) time / 1000;
+        main.setText("" + seconds);
+
         long entireTime = time;
 
         time = System.currentTimeMillis();
         MyList subCaves = cMapper.subCaves();
         time = System.currentTimeMillis() - time;
-        d.drawRooms(Color.GREEN, subCaves);
 
-        System.out.println("sub-room buid time: " + time);
+        seconds = (double) time / 1000;
+        sub.setText("" + seconds);
         entireTime += time;
-        System.out.println("entire time: " + entireTime);
-
-        //Paths paths = new Paths(mainCaves, subCaves, deep);
-       
-        d.drawPaths(mainCaves, Color.BLUE);
-        d.drawPaths(subCaves, Color.PINK);
-
-        try {
-            Thread.sleep(10000);
-        } catch (Exception e) {
-            System.out.println(e);
+        seconds = (double) entireTime / 1000;
+        total.setText("" + seconds);
+        if (draw) {
+            Draw d = new Draw(1920, 1200, "Cavern");
+            d.drawRooms(Color.RED, mainCaves);
+            d.drawRooms(Color.GREEN, subCaves);
+            d.drawPaths(mainCaves, Color.BLUE);
+            d.drawPaths(subCaves, Color.PINK);
         }
-        System.exit(0);
     }
+
 }
